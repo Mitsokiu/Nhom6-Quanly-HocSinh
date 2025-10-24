a257a929807cac69af38c23f4041abeb25da9078
ngnhuan282 <nguyennhuan2003@gmail.com>
2025-10-24 20:55:55 +0700

    a

--- Studenapp/obj/Debug/net8.0-windows/Studenapp.AssemblyInfo.cs
+++ Studenapp/obj/Debug/net8.0-windows/Studenapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Studenapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4de5a8f4e1b5e35567e03b250e67bcbc8dab317b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca5d36bb3306d13d2519e1ff4fa0edf4b3566cc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Studenapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Studenapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
