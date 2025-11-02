33b1a77f879465a010ca264650523fb950d4f35e
QuanLe5704 <lekhacquan5704@gmail.com>
2025-11-02 12:49:21 +0700

    Sua lai file LoginForm

--- Studenapp/Forms/LoginForm.cs
+++ Studenapp/Forms/LoginForm.cs
@@ -11,56 +11,51 @@ namespace StudentMan
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            // TODO: xử lý đăng nhập ở đây
-            string user = textBox1.Text;
-            string pass = textBox2.Text;
+       private void button1_Click(object sender, EventArgs e)
+{
+    string user = textBox1.Text;
+    string pass = textBox2.Text;
 
-            if (user == "admin" && pass == "123")
-            {
-                MessageBox.Show(this, "Đăng nhập thành công!");
-                // Ví dụ: mở form Admin
-                AdminForm main = new AdminForm();
-                main.Show();
-                this.Hide();
-            }
-            if (user == "GVBM" && pass == "123")
-            {
-                MessageBox.Show(this, "Đăng nhập thành công!");
-                // Ví dụ: mở form Admin
-                GiaoVienBoMon main = new GiaoVienBoMon();
-                main.Show();
-                this.Hide();
-            }
-            if (user == "HS" && pass == "123")
-            {
-                MessageBox.Show(this, "Đăng nhập thành công!");
-                // Ví dụ: mở form Admin
-                HocSinh main = new HocSinh();
-                main.Show();
-                this.Hide();
-            }
-            if (user == "PH" && pass == "123")
-            {
-                MessageBox.Show(this, "Đăng nhập thành công!");
-                // Ví dụ: mở form Admin
-                PhuHuynh main = new PhuHuynh();
-                main.Show();
-                this.Hide();
-            }
-            if (user == "GVCN" && pass == "123")
-            {
-                MessageBox.Show(this,"Đăng nhập thành công!");
-                // Ví dụ: mở form Admin
-                GiaoVienChuNhiem main = new GiaoVienChuNhiem();
-                main.Show();
-                this.Hide();
-            }
-            else
-            {
-                MessageBox.Show("Sai tài khoản hoặc mật khẩu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-        }
+    if (user == "admin" && pass == "123")
+    {
+        MessageBox.Show(this, "Đăng nhập thành công!");
+        AdminForm main = new AdminForm();
+        main.Show();
+        this.Hide();
+    }
+    else if (user == "GVBM" && pass == "123")
+    {
+        MessageBox.Show(this, "Đăng nhập thành công!");
+        GiaoVienBoMon main = new GiaoVienBoMon();
+        main.Show();
+        this.Hide();
+    }
+    else if (user == "HS" && pass == "123")
+    {
+        MessageBox.Show(this, "Đăng nhập thành công!");
+        HocSinh main = new HocSinh();
+        main.Show();
+        this.Hide();
+    }
+    else if (user == "PH" && pass == "123")
+    {
+        MessageBox.Show(this, "Đăng nhập thành công!");
+        PhuHuynh main = new PhuHuynh();
+        main.Show();
+        this.Hide();
+    }
+    else if (user == "GVCN" && pass == "123")
+    {
+        MessageBox.Show(this, "Đăng nhập thành công!");
+        GiaoVienChuNhiem main = new GiaoVienChuNhiem();
+        main.Show();
+        this.Hide();
+    }
+    else
+    {
+        MessageBox.Show("Sai tài khoản hoặc mật khẩu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+    }
+}
+
     }
 }
