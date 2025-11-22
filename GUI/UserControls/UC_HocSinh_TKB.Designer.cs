39b8539accf3c4b0886890097882770ee2438d45
ngnhuan282 <nguyennhuan2003@gmail.com>
2025-11-23 00:52:29 +0700

    update

--- GUI/UserControls/UC_HocSinh_TKB.Designer.cs
+++ GUI/UserControls/UC_HocSinh_TKB.Designer.cs
@@ -1,117 +1,151 @@
-﻿using System;
+﻿using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
-namespace GUI.UserControls
-{
-    partial class UC_HocSinh_TKB
-    {
-        /// <summary> 
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
 
-        /// <summary> 
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+namespace GUI
+{
+    partial class ThoiKhoaBieuControl
+    {
+        private IContainer components = null;
+
+        // Header
+        private Panel panelHeader;
+        private Label lblTitle;
+
+        // Wrapper chứa cụm chọn tuần
+        private Panel panelWeekWrapper;
+        private Label lblWeekTitle;
+        public ComboBox cboWeek;
+
+        // Main Layout
+        private Panel panelContainer;
+        private TableLayoutPanel tblTimetable;
+
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
+            if (disposing && (components != null)) components.Dispose();
             base.Dispose(disposing);
         }
 
         #region Component Designer generated code
 
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            dataGridView1 = new DataGridView();
-            Thu2 = new DataGridViewTextBoxColumn();
-            Thu3 = new DataGridViewTextBoxColumn();
-            Thu4 = new DataGridViewTextBoxColumn();
-            Thu5 = new DataGridViewTextBoxColumn();
-            Thu6 = new DataGridViewTextBoxColumn();
-            Thu7 = new DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-            SuspendLayout();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelWeekWrapper = new System.Windows.Forms.Panel();
+            this.lblWeekTitle = new System.Windows.Forms.Label();
+            this.cboWeek = new System.Windows.Forms.ComboBox();
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.tblTimetable = new System.Windows.Forms.TableLayoutPanel();
+
+            this.panelHeader.SuspendLayout();
+            this.panelWeekWrapper.SuspendLayout();
+            this.panelContainer.SuspendLayout();
+            this.SuspendLayout();
+
             // 
-            // dataGridView1
+            // ThoiKhoaBieuControl
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Thu2, Thu3, Thu4, Thu5, Thu6, Thu7 });
-            dataGridView1.Location = new Point(20, 15);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(919, 496);
-            dataGridView1.TabIndex = 0;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.Padding = new System.Windows.Forms.Padding(20);
+            this.Size = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.panelContainer);
+            this.Controls.Add(this.panelHeader);
+
             // 
-            // Thu2
+            // panelHeader
             // 
-            Thu2.HeaderText = "Thứ 2";
-            Thu2.MinimumWidth = 8;
-            Thu2.Name = "Thu2";
-            Thu2.Width = 150;
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Height = 60;
+            this.panelHeader.Controls.Add(this.panelWeekWrapper);
+            this.panelHeader.Controls.Add(this.lblTitle);
+
             // 
-            // Thu3
+            // panelWeekWrapper
             // 
-            Thu3.HeaderText = "Thứ 3";
-            Thu3.MinimumWidth = 8;
-            Thu3.Name = "Thu3";
-            Thu3.Width = 150;
+            this.panelWeekWrapper.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelWeekWrapper.Width = 450;
+            this.panelWeekWrapper.Controls.Add(this.cboWeek);
+            this.panelWeekWrapper.Controls.Add(this.lblWeekTitle);
+
             // 
-            // Thu4
+            // lblTitle
             // 
-            Thu4.HeaderText = "Thứ 4";
-            Thu4.MinimumWidth = 8;
-            Thu4.Name = "Thu4";
-            Thu4.Width = 150;
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(33, 37, 41);
+            this.lblTitle.Location = new System.Drawing.Point(0, 5);
+            this.lblTitle.Text = "Thời Khóa Biểu";
+
             // 
-            // Thu5
+            // lblWeekTitle
             // 
-            Thu5.HeaderText = "Thứ 5";
-            Thu5.MinimumWidth = 8;
-            Thu5.Name = "Thu5";
-            Thu5.Width = 150;
+            this.lblWeekTitle.AutoSize = true;
+            this.lblWeekTitle.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblWeekTitle.ForeColor = System.Drawing.Color.FromArgb(73, 80, 87);
+            this.lblWeekTitle.Location = new System.Drawing.Point(40, 18);
+            this.lblWeekTitle.Text = "Chọn tuần:";
+
             // 
-            // Thu6
+            // cboWeek
             // 
-            Thu6.HeaderText = "Thứ 6";
-            Thu6.MinimumWidth = 8;
-            Thu6.Name = "Thu6";
-            Thu6.Width = 150;
+            this.cboWeek.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboWeek.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cboWeek.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cboWeek.FormattingEnabled = true;
+            this.cboWeek.Location = new System.Drawing.Point(125, 15);
+            this.cboWeek.Size = new System.Drawing.Size(310, 28);
+            this.cboWeek.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+
             // 
-            // Thu7
+            // panelContainer
             // 
-            Thu7.HeaderText = "Thứ 7";
-            Thu7.MinimumWidth = 8;
-            Thu7.Name = "Thu7";
-            Thu7.Width = 150;
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.panelContainer.Controls.Add(this.tblTimetable);
+
             // 
-            // UC_HocSinh_TKB
+            // tblTimetable
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
-            AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(dataGridView1);
-            Name = "UC_HocSinh_TKB";
-            Size = new Size(951, 547);
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-            ResumeLayout(false);
+            this.tblTimetable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblTimetable.BackColor = System.Drawing.Color.White;
+            this.tblTimetable.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tblTimetable.ForeColor = System.Drawing.Color.FromArgb(222, 226, 230);
+            this.tblTimetable.ColumnCount = 7;
+            this.tblTimetable.RowCount = 6;
+
+            // Clean styles
+            this.tblTimetable.ColumnStyles.Clear();
+            this.tblTimetable.RowStyles.Clear();
+
+            // Setup Column Styles (Thủ công để tránh lỗi Designer)
+            this.tblTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F)); // Cot Tiet
+            this.tblTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66F));
+            this.tblTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66F));
+            this.tblTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66F));
+            this.tblTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66F));
+            this.tblTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66F));
+            this.tblTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66F));
+
+            // Setup Row Styles
+            this.tblTimetable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F)); // Header Row
+            this.tblTimetable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tblTimetable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tblTimetable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tblTimetable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tblTimetable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.panelWeekWrapper.ResumeLayout(false);
+            this.panelWeekWrapper.PerformLayout();
+            this.panelContainer.ResumeLayout(false);
+            this.tblTimetable.ResumeLayout(false);
+            this.ResumeLayout(false);
         }
 
         #endregion
-
-        private DataGridView dataGridView1;
-        private DataGridViewTextBoxColumn Thu2;
-        private DataGridViewTextBoxColumn Thu3;
-        private DataGridViewTextBoxColumn Thu4;
-        private DataGridViewTextBoxColumn Thu5;
-        private DataGridViewTextBoxColumn Thu6;
-        private DataGridViewTextBoxColumn Thu7;
     }
 }
--- GUI/UserControls/UC_HocSinh_TKB.cs
+++ GUI/UserControls/UC_HocSinh_TKB.cs
@@ -1,20 +1,250 @@
-﻿using System;
+﻿using BUS; // Gọi tầng BUS
+using DTO; // Gọi tầng DTO
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
+using System.Reflection;
 using System.Windows.Forms;
 
-namespace GUI.UserControls
+namespace GUI
 {
-    public partial class UC_HocSinh_TKB : UserControl
+    public partial class ThoiKhoaBieuControl : UserControl
     {
-        public UC_HocSinh_TKB()
+        // GIẢ LẬP: ID lớp học hiện tại.
+        // Trong thực tế, khi đăng nhập xong bạn sẽ lấy ClassID của học sinh đó lưu vào biến toàn cục.
+        // Theo file db.sql của bạn, học sinh id=1 đang học lớp id=1.
+        private int currentClassId = 1;
+
+        public ThoiKhoaBieuControl()
         {
             InitializeComponent();
+
+            // 1. Tối ưu hiệu suất
+            ConfigureDoubleBuffering();
+
+            // 2. Vẽ khung Header (Thứ/Tiết)
+            InitTableHeaders();
+
+            // 3. Nạp danh sách Học kỳ vào ComboBox và tải dữ liệu
+            LoadSemesters();
+        }
+
+        private void ConfigureDoubleBuffering()
+        {
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true);
+            this.UpdateStyles();
+            if (this.tblTimetable != null)
+            {
+                typeof(Control).InvokeMember("DoubleBuffered",
+                    BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+                    null, this.tblTimetable, new object[] { true });
+            }
+        }
+
+        // --- HEADER GIAO DIỆN ---
+        private void InitTableHeaders()
+        {
+            this.tblTimetable.SuspendLayout();
+            // Header Hàng ngang
+            AddHeaderLabel("Tiết", 0, 0);
+            AddHeaderLabel("Thứ Hai", 1, 0);
+            AddHeaderLabel("Thứ Ba", 2, 0);
+            AddHeaderLabel("Thứ Tư", 3, 0);
+            AddHeaderLabel("Thứ Năm", 4, 0);
+            AddHeaderLabel("Thứ Sáu", 5, 0);
+            AddHeaderLabel("Thứ Bảy", 6, 0);
+
+            // Header Cột dọc
+            AddPeriodLabel("Tiết 1\n7:00 - 7:45", 0, 1);
+            AddPeriodLabel("Tiết 2\n8:00 - 8:45", 0, 2);
+            AddPeriodLabel("Tiết 3\n9:00 - 9:45", 0, 3);
+            AddPeriodLabel("Tiết 4\n10:00 - 10:45", 0, 4);
+            AddPeriodLabel("Tiết 5\n11:00 - 11:45", 0, 5);
+            this.tblTimetable.ResumeLayout();
+        }
+
+        // --- TẢI DỮ LIỆU TỪ DB ---
+
+        private void LoadSemesters()
+        {
+            // Nạp danh sách học kỳ vào ComboBox
+            // (Lẽ ra nên load từ bảng `semesters`, nhưng ở đây mình tạo cứng cho khớp với data mẫu của bạn)
+            cboWeek.Items.Clear();
+
+            // Dùng class vô danh để lưu Text và Value
+            cboWeek.DisplayMember = "Text";
+            cboWeek.ValueMember = "Value";
+
+            cboWeek.Items.Add(new { Text = "Học kỳ 1 (2024-2025)", Value = 1 });
+            cboWeek.Items.Add(new { Text = "Học kỳ 2 (2024-2025)", Value = 2 });
+
+            // Sự kiện khi chọn học kỳ -> Load lại TKB
+            cboWeek.SelectedIndexChanged += (s, e) => LoadTimetableFromDB();
+
+            // Mặc định chọn cái đầu tiên
+            if (cboWeek.Items.Count > 0) cboWeek.SelectedIndex = 0;
+        }
+
+        private void LoadTimetableFromDB()
+        {
+            if (cboWeek.SelectedItem == null) return;
+
+            // Lấy SemesterID từ ComboBox
+            dynamic selectedItem = cboWeek.SelectedItem;
+            int semesterId = selectedItem.Value;
+
+            this.tblTimetable.SuspendLayout();
+            try
+            {
+                // 1. Xóa các môn học cũ trên bảng (Giữ lại Header)
+                // Duyệt ngược để xóa an toàn. Chỉ xóa Panel (là thẻ môn học), không xóa Label (là Header)
+                for (int i = tblTimetable.Controls.Count - 1; i >= 0; i--)
+                {
+                    Control c = tblTimetable.Controls[i];
+                    if (c is Panel)
+                    {
+                        tblTimetable.Controls.RemoveAt(i);
+                    }
+                }
+
+                // 2. GỌI BUS ĐỂ LẤY LIST DỮ LIỆU
+                List<TimetableDTO> listTKB = TimetableBUS.Instance.GetTimetable(currentClassId, semesterId);
+
+                // 3. Vẽ dữ liệu lên bảng
+                foreach (var item in listTKB)
+                {
+                    // Database lưu: "Mon", "Tue"... -> Cần đổi sang cột 1, 2...
+                    int col = ConvertDayToColumn(item.Day);
+                    int row = item.Period; // Tiết 1 -> Hàng 1
+
+                    if (col != -1 && row >= 1 && row <= 5)
+                    {
+                        // Tự động chọn màu theo tên môn
+                        SubjectColor color = GetColorBySubjectName(item.SubjectName);
+
+                        AddSubject(col, row, item.SubjectName, "GV: " + item.TeacherName, "Phòng: " + item.Room, color);
+                    }
+                }
+            }
+            finally
+            {
+                this.tblTimetable.ResumeLayout();
+            }
+        }
+
+        // Hàm chuyển đổi "Mon" -> Cột 1
+        private int ConvertDayToColumn(string day)
+        {
+            switch (day.Trim()) // Trim để xóa khoảng trắng thừa nếu có trong DB
+            {
+                case "Mon": return 1;
+                case "Tue": return 2;
+                case "Wed": return 3;
+                case "Thu": return 4;
+                case "Fri": return 5;
+                case "Sat": return 6;
+                default: return -1;
+            }
+        }
+
+        // --- VẼ GIAO DIỆN ---
+
+        private void AddSubject(int col, int row, string subjectName, string teacherName, string roomName, SubjectColor colorType)
+        {
+            Panel cardPanel = new Panel { Dock = DockStyle.Fill, Margin = new Padding(4), BackColor = Color.Transparent };
+            Color bgColor, textColor;
+            GetColorSchema(colorType, out bgColor, out textColor);
+
+            Label lblSubject = new Label { Text = subjectName, Font = new Font("Segoe UI", 10F, FontStyle.Bold), ForeColor = textColor, AutoSize = true, Location = new Point(10, 8), BackColor = Color.Transparent };
+            cardPanel.Controls.Add(lblSubject);
+
+            if (!string.IsNullOrEmpty(teacherName))
+            {
+                Label lblTeacher = new Label { Text = teacherName, Font = new Font("Segoe UI", 8.5F, FontStyle.Regular), ForeColor = textColor, AutoSize = true, Location = new Point(10, 30), BackColor = Color.Transparent };
+                cardPanel.Controls.Add(lblTeacher);
+            }
+            if (!string.IsNullOrEmpty(roomName))
+            {
+                Label lblRoom = new Label { Text = roomName, Font = new Font("Segoe UI", 8.5F, FontStyle.Regular), ForeColor = textColor, AutoSize = true, Location = new Point(10, 48), BackColor = Color.Transparent };
+                cardPanel.Controls.Add(lblRoom);
+            }
+
+            // Vẽ bo góc
+            cardPanel.Paint += (s, e) =>
+            {
+                e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                if (cardPanel.Width > 1 && cardPanel.Height > 1)
+                {
+                    using (GraphicsPath path = RoundedRect(new Rectangle(0, 0, cardPanel.Width - 1, cardPanel.Height - 1), 12))
+                    using (SolidBrush brush = new SolidBrush(bgColor)) e.Graphics.FillPath(brush, path);
+                }
+            };
+            this.tblTimetable.Controls.Add(cardPanel, col, row);
+        }
+
+        // Helper tạo Label Header
+        private void AddHeaderLabel(string text, int col, int row)
+        {
+            Label lbl = new Label { Text = text, Dock = DockStyle.Fill, TextAlign = ContentAlignment.MiddleCenter, Font = new Font("Segoe UI", 10F, FontStyle.Bold), ForeColor = Color.FromArgb(73, 80, 87), BackColor = Color.White, Margin = new Padding(0) };
+            this.tblTimetable.Controls.Add(lbl, col, row);
+        }
+
+        private void AddPeriodLabel(string text, int col, int row)
+        {
+            Label lbl = new Label { Text = text, Dock = DockStyle.Fill, TextAlign = ContentAlignment.MiddleCenter, Font = new Font("Segoe UI", 9F, FontStyle.Bold), ForeColor = Color.Black, BackColor = Color.White, Margin = new Padding(0) };
+            this.tblTimetable.Controls.Add(lbl, col, row);
+        }
+
+        // --- MÀU SẮC ---
+        public enum SubjectColor { Red, Blue, Orange, Green, Purple, Yellow, Cyan, Gray }
+
+        // Chọn màu dựa trên tên môn học
+        private SubjectColor GetColorBySubjectName(string name)
+        {
+            name = name.ToLower();
+            if (name.Contains("toán")) return SubjectColor.Red;
+            if (name.Contains("lý") || name.Contains("vật lý")) return SubjectColor.Blue;
+            if (name.Contains("hóa")) return SubjectColor.Green;
+            if (name.Contains("văn") || name.Contains("ngữ văn")) return SubjectColor.Orange;
+            if (name.Contains("anh") || name.Contains("tiếng anh")) return SubjectColor.Purple;
+            if (name.Contains("sử") || name.Contains("địa")) return SubjectColor.Yellow;
+            if (name.Contains("thể") || name.Contains("giáo dục")) return SubjectColor.Cyan;
+            return SubjectColor.Gray;
+        }
+
+        private void GetColorSchema(SubjectColor type, out Color bg, out Color text)
+        {
+            switch (type)
+            {
+                case SubjectColor.Red: bg = Color.FromArgb(255, 235, 238); text = Color.FromArgb(183, 28, 28); break;
+                case SubjectColor.Blue: bg = Color.FromArgb(227, 242, 253); text = Color.FromArgb(13, 71, 161); break;
+                case SubjectColor.Orange: bg = Color.FromArgb(255, 243, 224); text = Color.FromArgb(230, 81, 0); break;
+                case SubjectColor.Green: bg = Color.FromArgb(232, 245, 233); text = Color.FromArgb(27, 94, 32); break;
+                case SubjectColor.Purple: bg = Color.FromArgb(243, 229, 245); text = Color.FromArgb(74, 20, 140); break;
+                case SubjectColor.Yellow: bg = Color.FromArgb(255, 253, 231); text = Color.FromArgb(245, 127, 23); break;
+                case SubjectColor.Cyan: bg = Color.FromArgb(224, 247, 250); text = Color.FromArgb(0, 96, 100); break;
+                default: bg = Color.FromArgb(241, 243, 245); text = Color.FromArgb(73, 80, 87); break;
+            }
+        }
+
+        private GraphicsPath RoundedRect(Rectangle bounds, int radius)
+        {
+            int diameter = radius * 2;
+            Size size = new Size(diameter, diameter);
+            Rectangle arc = new Rectangle(bounds.Location, size);
+            GraphicsPath path = new GraphicsPath();
+            if (radius == 0) { path.AddRectangle(bounds); return path; }
+            path.AddArc(arc, 180, 90);
+            arc.X = bounds.Right - diameter;
+            path.AddArc(arc, 270, 90);
+            arc.Y = bounds.Bottom - diameter;
+            path.AddArc(arc, 0, 90);
+            arc.X = bounds.Left;
+            path.AddArc(arc, 90, 90);
+            path.CloseFigure();
+            return path;
         }
     }
 }
