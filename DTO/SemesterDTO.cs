6619b5761c29dc86a4bea60a41a4763b798c56f2
ngnhuan282 <nguyennhuan2003@gmail.com>
2025-11-23 12:30:06 +0700

    Revert "Xem TKB, xem học phí"
    This reverts commit 7468ddb9e77c2683a0e59bfe8da6c4560ff0f676.

--- DTO/SemesterDTO.cs
+++ DTO/SemesterDTO.cs
@@ -6,12 +6,8 @@ namespace DTO
     {
         public int SemesterId { get; set; }
         public int YearId { get; set; }
-        public string SemesterName { get; set; }
-        public string YearName { get; set; }    
+        public string Name { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-
-        // Property phụ để hiển thị trên ComboBox
-        public string DisplayName => $"{SemesterName} - Năm học {YearName}";
     }
 }
