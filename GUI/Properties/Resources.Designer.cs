4357941ed58d7a28b0acfe4ef9ce8310a2f0e766
ngnhuan282 <nguyennhuan2003@gmail.com>
2025-12-05 23:12:52 +0700

    Cài thư viện iTextSharp hỗ trợ xuất PDF

--- GUI/Properties/Resources.Designer.cs
+++ GUI/Properties/Resources.Designer.cs
@@ -83,16 +83,6 @@ namespace GUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap avatar_macdinh {
-            get {
-                object obj = ResourceManager.GetObject("avatar_macdinh", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap dangxuat_24 {
             get {
                 object obj = ResourceManager.GetObject("dangxuat_24", resourceCulture);
@@ -273,9 +263,9 @@ namespace GUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap nhapexcel_48 {
+        internal static System.Drawing.Bitmap nhapngaynghi_24 {
             get {
-                object obj = ResourceManager.GetObject("nhapexcel_48", resourceCulture);
+                object obj = ResourceManager.GetObject("nhapngaynghi_24", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -283,9 +273,9 @@ namespace GUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap nhapngaynghi_24 {
+        internal static System.Drawing.Bitmap pdf_32 {
             get {
-                object obj = ResourceManager.GetObject("nhapngaynghi_24", resourceCulture);
+                object obj = ResourceManager.GetObject("pdf_32", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -396,16 +386,6 @@ namespace GUI.Properties {
         internal static System.Drawing.Bitmap xuatexcel_32 {
             get {
                 object obj = ResourceManager.GetObject("xuatexcel_32", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap xuatexcel_48 {
-            get {
-                object obj = ResourceManager.GetObject("xuatexcel_48", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
